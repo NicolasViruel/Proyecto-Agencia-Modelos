5bad5fade1d4c55f0e9c1bcaae497b6e32d02935
PerezFe <PerezFernandoE83@gmail.com>
2023-01-20 22:53:37 -0300

    trabajando en el detalle de las modelos

--- src/Components/Pages/Staff/Staff.jsx
+++ src/Components/Pages/Staff/Staff.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import "../Staff/staff.css";
 
 const Staff = () => {
   return (
-    <div>Staff</div>
-  )
-}
+    <div>
+      <h1 className="text-center my-3">Staff</h1>
+      <hr className="mx-4" />
+      <Card
+        style={{ width: "18rem" }}
+        className="m-5 img-fluid shadow p-4 mb-3 bg-white rounded"
+      >
+        <Card.Img
+          variant="top"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI6fqq_I5J2vx6indILC-iA_0qpipndzYxsgXg7Sc51A0NOX6533tJMdMiLLoq-RN74SA&usqp=CAU"
+        />
+        <Card.Body>
+          <Card.Title>Mariana Rojas</Card.Title>
+          <Card.Text>
+            ¡Mariana Rojas, con su aspecto audaz y su encanto de colegial, puede
+            hacer que tu corazón dé un vuelco!
+          </Card.Text>
+          <Button variant="primary">Ver detalle</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
 
-export default Staff
+export default Staff;
