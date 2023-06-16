6bb87a6f4cb097abcccee0959074168e2431de58
nicolas <nicolasviruel@gmail.com>
2023-06-15 22:51:58 -0300

    Se agregan cambios en las Jquerys para refactorizar las ruta de test por modelo

--- src/Components/Pages/Staff/Staff.jsx
+++ src/Components/Pages/Staff/Staff.jsx
@@ -1,19 +1,11 @@
 import React, { useEffect, useState } from "react";
 import CardModelo from "../Views/Modelos/CardModelo";
 import { consultarAPI } from "../../../helpers/queries";
-// import { getStudents} from "../../../helpers/students";
 import { Container, Row } from "react-bootstrap";
 import "../Staff/staff.css";
 
 const Staff = () => {
   const [modelos, setModelos] = useState([]);
-
-  // useEffect(() => {
-  //   getStudents().then((respuesta) => {
-  //     console.log(respuesta);
-  //     setModelos(respuesta);
-  //   });
-  // }, []);
 
   useEffect(() => {
     consultarAPI().then((respuesta) => {
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -1,5 +1,4 @@
 export const URL = "http://localhost:3004";
-export const URLA = "http://localhost:3004/alumnos";
 
 // Quiero ser Modelo Mensaje API
 export const quieroSerModeloAPI = async(mensaje)=>{
@@ -24,7 +23,7 @@ export const quieroSerModeloAPI = async(mensaje)=>{
 
 export const crearModeloAPI = async(modelo)=>{
     try{
-        const respuesta = await fetch(URL+"/"+"test"+"/"+"newcontact" ,{
+        const respuesta = await fetch(URL+"/"+"modelo"+"/"+"newcontact" ,{
             method: "POST",
             headers:{
                 "Content-Type":"application/json"
@@ -40,7 +39,7 @@ export const crearModeloAPI = async(modelo)=>{
 
 export const consultarAPI = async(modelo)=>{
     try{
-        const respuesta = await fetch(URL+"/"+"test");
+        const respuesta = await fetch(URL+"/"+"modelo");
         const listaModelos = await respuesta.json()
         return listaModelos
     }catch(error){
@@ -63,7 +62,7 @@ export const borrarModeloAPI = async(id)=>{
 
 export const obtenerModeloAPI = async(id)=>{
     try{
-        const respuesta = await fetch(URL+"/"+"test"+"/"+`${id}`);
+        const respuesta = await fetch(URL+"/"+"modelo"+"/"+`${id}`);
         const modeloBuscada = {
             dato: await respuesta.json(),
             status: respuesta.status
@@ -93,13 +92,6 @@ export const editarModeloAPI = async(id,datosActualizados)=>{
 // -------------------- Campus Alumnos API --------------------------------
 
 export const consultarAPIA = async()=>{
-    // try{
-    //     const respuesta = await fetch(URLA);
-    //     const listaAlumnos = await respuesta.json()
-    //     return listaAlumnos
-    // }catch(error){
-    //     console.log(error)
-    // }
     try{
         const respuesta = await fetch(URL+"/"+"api"+ "/" +"Alumnos");
         const listaModelos = await respuesta.json()
@@ -111,13 +103,14 @@ export const consultarAPIA = async()=>{
 
 export const crearAlumnoAPI = async(alumno)=>{
     try{
-        const respuesta = await fetch(URLA,{
+        const respuesta = await fetch(URL + "/" +"api" + "/" + "Alumnos" + "/" + "nuevoAlumno" ,{
             method: "POST",
             headers:{
                 "Content-Type":"application/json"
             },
-            body: JSON.stringify(alumno)
+            body: JSON.stringify(modelo)
         });
+        console.log(respuesta);
         return respuesta;
     }catch(error){
         console.log(error)
@@ -126,18 +119,21 @@ export const crearAlumnoAPI = async(alumno)=>{
 
 export const borrarAlumnoAPI = async(id)=>{
     try{
-        const respuesta = await fetch(`${URLA}/${id}`,{
-            method: "DELETE",
-        });
-        return respuesta;
+        const respuesta = await fetch(URL+ "/" +"api" + "/" + "Alumnos" + "/" +`${id}`);
+        const modeloBuscada = {
+            dato: await respuesta.json(),
+            status: respuesta.status
+        }
+        return modeloBuscada;
     }catch(error){
         console.log(error)
+    
     }
 }
 
 export const obtenerAlumnoAPI = async(id)=>{
     try{
-        const respuesta = await fetch(URLA+"/"+id);
+        const respuesta = await fetch(URL+"/"+id);
         const alumnoBuscado = {
             dato: await respuesta.json(),
             status: respuesta.status
@@ -150,7 +146,7 @@ export const obtenerAlumnoAPI = async(id)=>{
 
 export const editarAlumnoAPI = async(id,datosActualizados)=>{
     try{
-        const respuesta = await fetch(URLA+"/"+id,{
+        const respuesta = await fetch(URL + "/" +"api" + "/" + "Alumnos" + "/" +`${id}` ,{
             method: "PUT",
             headers:{
                 "Content-Type": "application/json"
@@ -163,4 +159,58 @@ export const editarAlumnoAPI = async(id,datosActualizados)=>{
     }
 }
 
-// Formulario Quiero ser Modelo API
+// Formulario Contacto
+
+export const consultarMensaje = async()=>{
+    try{
+        const respuesta = await fetch(URL+"/"+"api"+ "/" +"Contacto");
+        const listaMensaje = await respuesta.json()
+        return listaMensaje
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export const crearMensajeApi = async(mensaje)=>{
+    try{
+        const respuesta = await fetch(URL + "/" +"api" + "/" + "Contacto" + "/" + "nuevoMensaje" ,{
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(mensaje)
+        });
+        console.log(respuesta);
+        return respuesta;
+    }catch(error){
+        console.log(error)
+    }
+}
+
+// Formulario Quiero Ser modelo
+
+export const consultarQSM = async()=>{
+    try{
+        const respuesta = await fetch(URL+"/"+"api"+ "/" +"QuieroSM");
+        const listaMensaje = await respuesta.json()
+        return listaMensaje
+    }catch(error){
+        console.log(error)
+    }
+}
+
+export const crearQSM = async(mensaje)=>{
+    try{
+        const respuesta = await fetch(URL + "/" +"api" + "/" + "QuieroSM" + "/" + "nuevoMensaje" ,{
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(mensaje)
+        });
+        console.log(respuesta);
+        return respuesta;
+    }catch(error){
+        console.log(error)
+    }
+}
